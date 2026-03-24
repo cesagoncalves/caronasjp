8d73e1a0a3d992282163733178a3db6061c32c17
César Filho <juliocesar.patos@gmail.com>
2026-03-24 00:21:57 -0300

    correções 3

--- usuarios/middleware.py
+++ usuarios/middleware.py
@@ -7,31 +7,34 @@ class CompleteProfileMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        user = getattr(request, "user", None)
-        path = request.path or ""
+        try:
+            user = getattr(request, "user", None)
+            path = request.path or ""
 
-        if user and user.is_authenticated:
-            allowed_prefixes = (
-                "/admin/",
-                "/accounts/",
-                "/static/",
-                "/media/",
-            )
-            allowed_paths = {
-                reverse("login"),
-                reverse("logout"),
-                reverse("signup"),
-                reverse("alterar_senha"),
-                reverse("password_change_done"),
-                reverse("completar_perfil"),
-                reverse("push_subscribe"),
-                reverse("push_unsubscribe"),
-                reverse("push_skip"),
-                "/favicon.ico",
-            }
+            if user and user.is_authenticated:
+                allowed_prefixes = (
+                    "/admin/",
+                    "/accounts/",
+                    "/static/",
+                    "/media/",
+                )
+                allowed_paths = {
+                    reverse("login"),
+                    reverse("logout"),
+                    reverse("signup"),
+                    reverse("alterar_senha"),
+                    reverse("password_change_done"),
+                    reverse("completar_perfil"),
+                    reverse("push_subscribe"),
+                    reverse("push_unsubscribe"),
+                    reverse("push_skip"),
+                    "/favicon.ico",
+                }
 
-            if not path.startswith(allowed_prefixes) and path not in allowed_paths:
-                if user.socialaccount_set.exists() and (not user.nome_completo or not user.telefone):
-                    return redirect("completar_perfil")
+                if not path.startswith(allowed_prefixes) and path not in allowed_paths:
+                    if user.socialaccount_set.exists() and (not user.nome_completo or not user.telefone):
+                        return redirect("completar_perfil")
+        except Exception:
+            return self.get_response(request)
 
         return self.get_response(request)
